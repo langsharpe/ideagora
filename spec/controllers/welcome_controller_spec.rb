d370d45a9da302110859d8daf6b5b4d9c385d8e6
=Tom Ridge <=tom.ridge@bigpond.com>
2012-06-04 19:34:52 +1000

    small introductory tests for dashboard view

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,16 +1,21 @@
 require 'spec_helper'
 
 describe WelcomeController do
+ 
+  let(:camp) { Camp.current || Camp.make! }
+  let(:library) { Venue.make! :camp => camp }
+  let(:study) { Venue.make! :camp => camp }
+  
   describe '#index' do
-    let(:camp) { Camp.current || Camp.make! }
+    
         
     it "collects talks and groups them by venue" do
       pending 'changed in rcx version'
-      library = Venue.make! :camp => camp
+      
       Talk.make! :camp => camp, :venue => library, :start_at => Time.zone.now
       Talk.make! :camp => camp, :venue => library, :start_at => 1.minute.from_now
       
-      study = Venue.make! :camp => camp
+      
       Talk.make! :camp => camp, :venue => study, :start_at => Time.zone.now
       Talk.make! :camp => camp, :venue => study, :start_at => 2.days.from_now
       
@@ -20,6 +25,22 @@ describe WelcomeController do
       
       # assigns[:todays_talks_by_venue_id][library.id].length.should == 2
       # assigns[:todays_talks_by_venue_id][study.id].length.should   == 1
+    end
+  end
+  
+  describe "#dashboard" do
+    it "displays the page" do
+      camp
+      get :dashboard
+      response.should be_success
+    end
+    it "has all the current events and talks" do
+      Talk.make! :camp => camp, :venue => library, :start_at => Time.zone.now
+      Talk.make! :camp => camp, :venue => library, :start_at => 1.minute.from_now
+      Event.make! :camp => camp, :venue => study, :start_at => Time.zone.now
+      Event.make! :camp => camp, :venue => study, :start_at => 2.days.from_now
+      get :dashboard
+      assigns(:events_and_talks).should == 2
     end
   end
 end
