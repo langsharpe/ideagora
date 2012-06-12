b53f5ff6afc630f4da6e7afd73a1308661ec71ce
Chris Raethke <chris@codesoda.com>
2012-06-12 15:44:32 +1000

    Updated seeds and csv to reflect railscamp 11 attendees

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,39 +2,51 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
-rc10 = Camp.find_by_name('Railscamp X') || Camp.create(:name => 'Railscamp X', :location => 'Woodhouse, Piccadilly, Adelaide', :current => true, :time_zone => 'Adelaide', :start_at => '2012-01-13 16:00:00', :end_at => '2012-01-16 10:00:00')
-Time.zone = rc10.time_zone
+rc11 = Camp.find_by_name('Railscamp 11') || Camp.create(:name => 'Railscamp 11', :location => 'Gold Coast Hinterland, Queensland', :current => true, :time_zone => 'Brisbane', :start_at => '2012-01-13 15:30:00', :end_at => '2012-01-16 10:00:00')
+Time.zone = rc11.time_zone
 
 require 'csv'
-filename = File.join(Rails.root.to_s, 'db', 'seeds', 'rc10_attendees.csv')
-puts "Making sure we have some users for #{rc10.name}..."
+filename = File.join(Rails.root.to_s, 'db', 'seeds', 'attendees.csv')
+puts "Making sure we have some users for #{rc11.name}..."
 CSV.foreach(filename, :headers => true) do |row|
-  if rc10.users.find_by_email(row['email']).nil?
-    puts "Creating #{row['first_name']} #{row['last_name']}"
-    u = rc10.users.create!(
-      :first_name => row['first_name'],
-      :last_name => row['last_name'],
-      :email => row['email'],
-      :twitter => row['twitter']
+  if rc11.users.find_by_email(row['Email']).nil?
+    puts "Creating #{row['First Name']} #{row['Last Name']}"
+    u = rc11.users.create!(
+      :first_name => row['First Name'],
+      :last_name => row['Last Name'],
+      :email => row['Email'],
+      :twitter => row['Twitter']
     )
   end
 end
 puts "Done with users."
 
-%w(ben@hoskings.net
-christopher.darroch@gmail.com
+%w(ben.schwarz@gmail.com
+ben.schwarz@gmail.com
+chris@codesoda.com
+daniel@netfox.com
+jason@codespike.com
+kpitty@cockatoosoftware.com.au
+luke@lukehamilton.me
+nathan@nathanhoad.net
+nigel@logical.com.au
+pat@freelancing-gods.com
+richie@inspire9.com
+sean@kiiii.com
 sebastian@vonconrad.com
-me@keithpitt.com).each do |email|
+secoif@gmail.com
+tridge@internode.on.net
+).each do |email|
   puts "Setting #{email} as organiser"
-  User.find_by_email!(email).attendances.first.update_attribute(:organiser, true)
+  organiser = User.find_by_email(email)
+  if organiser
+    organiser.attendances.first.update_attribute(:organiser, true)
+  end
 end
 puts 'Done with organisers.'
 
-cd = User.find_by_email!('christopher.darroch@gmail.com')
-svc = User.find_by_email!('sebastian@vonconrad.com')
-ryanbigg = User.find_by_email!('me@ryanbigg.com')
-lachlanhardy = User.find_by_email!('lachlan@lachstock.com.au')
 
+=begin
 puts 'Creating Venues'
 ['Main Hall', 'Henry Rymill room', 'Safe Refuge Area (Rec Room)', 'Pem Fooks room', 'A. J. Peake room (small)', 'Old House Dining Room', 'Old House Lounge Room'].each { |v| rc10.venues.create!(:name => v) unless rc10.venues.find_by_name(v) }
 catering = Venue.find_by_name!('Main Hall')
@@ -63,8 +75,11 @@ events << {:start_at => "2012-01-16 11:00:00", :end_at => "2012-01-16 11:05:00",
 
 events.each { |e| Event.create!({:camp => rc10, :venue => catering, :user => svc}.merge(e)) }
 puts 'Events created.'
+=end
+
 
 puts 'Adding a welcome message'
+nr = User.find_by_email("nigel@logical.com.au")
 content = <<-EOS
 Kickin&rsquo; in the front seat
 Sittin&rsquo; in the back seat
@@ -84,6 +99,6 @@ Fun, fun, fun, fun
 Lookin&rsquo; forward to the weekend
 EOS
 
-Notice.create!(:title => 'Welcome to Woodhouse', :content => content, :camp => rc10, :user => cd)
+Notice.create!(:title => 'Welcome to Woodhouse', :content => content, :camp => rc11, :user => nr)
 puts 'Message created.'
 puts 'Done!'
