6ed800cb16e97c932db07f62fbf789bf74f489a8
Ben Hoskings <ben@hoskings.net>
2012-06-15 22:30:46 +1000

    Use assets:precompile instead of assets:precompile:primary, which isn't working on rails-3.1.0.

--- babushka-deps/deploy.rb
+++ babushka-deps/deploy.rb
@@ -1,3 +1,9 @@
 dep "on deploy", :old_id, :new_id, :branch, :env do
-  requires 'benhoskings:when path changed'.with('app/assets/', 'assets precompiled', old_id, new_id, env)
+  requires 'benhoskings:when path changed'.with('app/assets/', 'assets fully precompiled', old_id, new_id, env)
+end
+
+dep 'assets fully precompiled', :env, :deploying, :template => 'task' do
+  run {
+    shell "bundle exec rake assets:precompile RAILS_ENV=#{env}"
+  }
 end
