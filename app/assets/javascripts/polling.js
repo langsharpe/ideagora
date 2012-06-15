b227f51cb2c5710a4df34f1152849934232d4d52
Ben Hoskings <ben@hoskings.net>
2012-06-15 22:06:10 +1000

    Update every 10 seconds.

--- app/assets/javascripts/polling.js
+++ app/assets/javascripts/polling.js
@@ -1,10 +1,10 @@
 $(function () {  
   if ($('#dashboard-events').length > 0 && $('#upcoming-events').length > 0) {  
-    setTimeout(updateEvents, 300000);  
+    setTimeout(updateEvents, 10000);
   }  
 });  
   
 function updateEvents() {  
   $.getScript('/welcome/dashboard.js');  
-  setTimeout(updateEvents, 300000);  
+  setTimeout(updateEvents, 10000);
 }
